ea0126334bca42734c2674b9222c7d8badb88cd9
Bruno Oyamada <bruno.oyamada@hotmail.com>
2026-08-31 17:10:40 -0300

    333. Chamando Funções da Camada Nativa via Flutter

--- android/app/src/main/java/br/com/floating_button/floating_button/MainActivity.java
+++ android/app/src/main/java/br/com/floating_button/floating_button/MainActivity.java
@@ -1,8 +1,14 @@
 package br.com.floating_button.floating_button;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.plugin.common.MethodChannel;
+
+import com.yhao.floatwindow.FloatWindow;
+import com.yhao.floatwindow.Screen;
+
 
 public class MainActivity extends FlutterActivity {
     private static final String CHANNEL = "floating_button";
@@ -16,8 +22,36 @@ public class MainActivity extends FlutterActivity {
             CHANNEL
         );
 
-        channel.setMethodCallHandler((call, result) -> {
-            
-        });
+        channel.setMethodCallHandler(
+            (call, result) -> {
+                switch(call.method) {
+                    case "create":
+                        ImageView imageView = new ImageView(getApplicationContext());
+                        imageView.setImageResource(R.drawable.plus);
+
+                        FloatWindow.with(getApplicationContext()).setView(imageView)
+                        .setWidth(Screen.width, 0.15f)
+                        .setHeight(Screen.width, 0.15f)
+                        .setX(Screen.width, 0.8f)
+                        .setY(Screen.height, 0.3f)
+                        .setDesktopShow(true)
+                        .build();
+
+                        result.success(null);
+                        break;
+                    case "show":
+                        FloatWindow.get().show();
+                        result.success(null);
+                        break;
+                    case "hide":
+                        FloatWindow.get().hide();
+                        result.success(null);
+                        break;
+                    default:
+                        result.notImplemented();
+                        break;
+                }
+            }
+        );
     }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -30,9 +30,34 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   static const platform = const MethodChannel("floating_button");
+
+  int count = 0;
   
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Floating Button Demo"),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text("$count", textAlign: TextAlign.center, style: TextStyle(fontSize: 50)), 
+            ElevatedButton(child: Text("Create"), onPressed: (){
+              platform.invokeMethod("create");
+            },),
+            ElevatedButton(onPressed: () {
+              platform.invokeMethod("show");
+            }, child: Text("Show")),
+            ElevatedButton(onPressed: () {
+              platform.invokeMethod("hide");
+            }, child: Text("Hide"))
+          ],
+        )
+      ),
+    );
   }
 }
